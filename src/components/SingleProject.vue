b12de122e8b389e43f492e3d3a34145239d7e57f
gnoul_ <ttluong4869@gmail.com>
2022-03-16 13:59:35 +0700

    Create Navbar

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="project" :class="{ complete: project.complete }">
+        <div class="actions">
+            <h3 @click="toggleDetails">{{ project.title }}</h3>
+            <div class="icons">
+                <span class="material-icons"> edit </span>
+                <span class="material-icons" @click="deleteProject"> delete </span>
+                <span class="material-icons tick" @click="toggleComplete"> done </span>
+            </div>
+        </div>
+        <div v-if="isDetailsShown" class="details">
+            <p>{{ project.details }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['project'],
+    data() {
+        return {
+            isDetailsShown: false,
+            uri: 'http://localhost:3000/projects/' + this.project.id,
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.isDetailsShown = !this.isDetailsShown;
+        },
+        deleteProject() {
+            fetch(this.uri, { method: 'DELETE' })
+                .then(() => this.$emit('delete', this.project.id))
+                .catch((err) => console.err(err));
+        },
+        toggleComplete() {
+            fetch(this.uri, {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ complete: !this.project.complete }),
+            })
+                .then(() => this.$emit('complete', this.project.id))
+                .catch((err) => console.err(err));
+        },
+    },
+};
+</script>
+
+<style>
+.project {
+    margin: 20px auto;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 4px;
+    box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.5);
+    border-left: 4px solid #e90074;
+}
+h3 {
+    cursor: pointer;
+}
+.actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.material-icons {
+    font-size: 24px;
+    margin-left: 10px;
+    color: #bbb;
+    cursor: pointer;
+}
+
+.material-icons:hover {
+    color: #777;
+}
+
+.project.complete {
+    border-left: 4px solid #00ce89;
+}
+.project.complete .tick {
+    color: #00ce89;
+}
+</style>
